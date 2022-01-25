1e2650b3067e0007f32b7490c0ba8ceb69b0f41a
Andrew Lam <alam1324@gmail.com>
2022-01-25 15:20:27 -0800

    Search show name

--- server/models.js
+++ server/models.js
@@ -1,34 +1,40 @@
 const axios = require("axios");
 
-const API = 'https://api.themoviedb.org/3/tv'
+const API = "https://api.themoviedb.org/3";
 
 const searchShow = async (req, res) => {
-  // search show name to get show ID
-
-  const tvId = "1405";
-  // search show ID to get seasons
-
-  const { data } = await axios.get(
-    `${API}/${tvId}?api_key=${process.env.API_KEY}`
-  );
-  const seasons = Array.from(
-    { length: data.number_of_seasons },
-    (v, k) => k + 1
-  );
-
-  // search season to get episode names, date, rating, vote count
+  const showName = "Game of Thrones";
 
   try {
+    // search show name to get show ID
+    const show = await axios.get(
+      `${API}/search/tv?api_key=${process.env.API_KEY}&query=${showName}`
+    );
+
+    const tvId = show.data.results[0].id;
+
+    // search show ID to get seasons
+    const { data } = await axios.get(
+      `${API}/tv/${tvId}?api_key=${process.env.API_KEY}`
+    );
+
+    // generate array of season numbers
+    const seasons = Array.from(
+      { length: data.number_of_seasons },
+      (v, k) => k + 1
+    );
+
+    // fetch all season data asynchronously
     (async () => {
       let episodes = {};
 
-      // fetch all season data asynchronously
       await Promise.all(
         seasons.map(async (season) => {
           const { data } = await axios.get(
-            `${API}/${tvId}/season/${season}?api_key=${process.env.API_KEY}`
+            `${API}/tv/${tvId}/season/${season}?api_key=${process.env.API_KEY}`
           );
           data.episodes.forEach((ep) => {
+
             const epNum = `${ep.season_number}.${String(
               ep.episode_number
             ).padStart(2, "0")}`;
@@ -44,7 +50,8 @@ const searchShow = async (req, res) => {
 
       // return sorted episode data
       res.send(episodes);
-    })();
+    }
+  )();
   } catch (e) {
     res.status(500).send(e);
   }
