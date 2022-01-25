df47e2132329dbcf2d298c7d1bfc6e7928af4ebe
Andrew Lam <alam1324@gmail.com>
2022-01-25 15:01:53 -0800

    Sort episodes

--- server/models.js
+++ server/models.js
@@ -1,30 +1,45 @@
-const axios = require('axios');
+const axios = require("axios");
+
+const API = 'https://api.themoviedb.org/3/tv'
 
 const searchShow = async (req, res) => {
   // search show name to get show ID
 
-  const tvId = '1405';
+  const tvId = "1405";
   // search show ID to get seasons
+
+  const { data } = await axios.get(
+    `${API}/${tvId}?api_key=${process.env.API_KEY}`
+  );
+  const seasons = Array.from(
+    { length: data.number_of_seasons },
+    (v, k) => k + 1
+  );
 
   // search season to get episode names, date, rating, vote count
 
-  const seasons = [1, 2, 3, 4, 5, 6, 7, 8];
-  let episodes = {};
-
   try {
     (async () => {
+      let episodes = {};
+
       // fetch all season data asynchronously
-      await Promise.all(seasons.map(async (season) => {
-        const { data } = await axios.get(`https://api.themoviedb.org/3/tv/${tvId}/season/${season}?api_key=${process.env.API_KEY}`);
-        data.episodes.forEach((ep) => {
-          const epNum = `${ep.season_number}.${String(ep.episode_number).padStart(2, '0')}`;
-          episodes[epNum] = ep.vote_average;
-        });
-      }));
+      await Promise.all(
+        seasons.map(async (season) => {
+          const { data } = await axios.get(
+            `${API}/${tvId}/season/${season}?api_key=${process.env.API_KEY}`
+          );
+          data.episodes.forEach((ep) => {
+            const epNum = `${ep.season_number}.${String(
+              ep.episode_number
+            ).padStart(2, "0")}`;
+            episodes[epNum] = ep.vote_average;
+          });
+        })
+      );
 
       // sort episodes in ascending order
       episodes = Object.fromEntries(
-        Object.entries(episodes).sort( (a,b) => a[0] - b[0] )
+        Object.entries(episodes).sort((a, b) => a[0] - b[0])
       );
 
       // return sorted episode data
